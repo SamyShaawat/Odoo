5815870bcb1d1b0014c79f9fa2f3e455af6f567d
Samy Mostafa Shaawat <samy.shaawat@gmail.com>
2025-03-19 22:15:32 +0200

    add new fields and make patient name required

--- odoo-12.0/addons/om_hospital/models/patient.py
+++ odoo-12.0/addons/om_hospital/models/patient.py
@@ -5,10 +5,10 @@ class HospitalPatient(models.Model):
     _name = 'hospital.patient'
     _description = 'Patient Record'
 
-    # patient_name = fields.Char(string='Name', required=True)
-    # patient_age = fields.Integer('Age')
-    # notes = fields.Text(string="Notes")
-    # image = fields.Binary(string="Image")
+    patient_name = fields.Char(string='Name', required=True)
+    patient_age = fields.Integer('Age')
+    notes = fields.Text(string="Notes")
+    image = fields.Binary(string="Image")
 
 
 
