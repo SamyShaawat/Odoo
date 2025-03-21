418be53073490b0337eb0b7279cf8fe040f645f6
Samy Mostafa Shaawat <samy.shaawat@gmail.com>
2025-03-21 16:30:28 +0200

    Add rec name for Models in Odoo Development

--- odoo-12.0/addons/om_hospital/models/patient.py
+++ odoo-12.0/addons/om_hospital/models/patient.py
@@ -4,11 +4,14 @@ from odoo import models, fields
 class HospitalPatient(models.Model):
     _name = 'hospital.patient'
     _description = 'Patient Record'
+    _rec_name = 'patient_name'
 
     patient_name = fields.Char(string='Name', required=True)
     patient_age = fields.Integer('Age')
     notes = fields.Text(string="Notes")
     image = fields.Binary(string="Image")
+    name = fields.Char(string="Test")
+
 
 
 
