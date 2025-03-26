0e638b9eb842a04b882a788935c7f44352bc2e38
Samy Mostafa Shaawat <samy.shaawat@gmail.com>
2025-03-26 23:19:47 +0200

    Define a Compute Field in Odoo 12

--- odoo-12.0/addons/om_hospital/models/patient.py
+++ odoo-12.0/addons/om_hospital/models/patient.py
@@ -15,31 +15,7 @@ class HospitalPatient(models.Model):
     _description = "Patient Record"
     _rec_name = "patient_name"
 
-    patient_name = fields.Char(string="Name", required=True)
-    patient_age = fields.Integer("Age")
-    notes = fields.Text(string="Notes")
-    image = fields.Binary(string="Image")
-    name = fields.Char(string="Test")
-    name_seq = fields.Char(
-        string="Patient ID",
-        required=True,
-        copy=False,
-        readonly=True,
-        index=True,
-        default=lambda self: _("New"),
-    )
-    gender = fields.Selection(
-        [
-            ("male", "Male"),
-            ("fe_male", "Female"),
-        ],
-        default="male",
-        string="Gender",
-    )
-    new_name = fields.Many2one(comodel_name="res.partner", string="New Name")
-
-    sale_order_count = fields.Integer(string="No. of Sale Orders", readonly=True)
-
+    # task from fathy
     @api.onchange("new_name")
     def _onchange_new_name(self):
         if self.new_name:
@@ -58,3 +34,48 @@ class HospitalPatient(models.Model):
             ) or _("New")
         result = super(HospitalPatient, self).create(vals)
         return result
+
+    # How to Write Compute Field and its Function in Odoo12
+    @api.depends("patient_age")
+    def set_age_group(self):
+        for rec in self:
+            if rec.patient_age:
+                if rec.patient_age < 18:
+                    rec.age_group = "minor"
+                else:
+                    rec.age_group = "major"
+
+    patient_name = fields.Char(string="Name", required=True)
+    patient_age = fields.Integer("Age")
+    notes = fields.Text(string="Registration Note")
+    image = fields.Binary(string="Image", attachment=True)
+    name = fields.Char(string="Test")
+    name_seq = fields.Char(
+        string="Patient ID",
+        required=True,
+        copy=False,
+        readonly=True,
+        index=True,
+        default=lambda self: _("New"),
+    )
+    gender = fields.Selection(
+        [
+            ("male", "Male"),
+            ("fe_male", "Female"),
+        ],
+        default="male",
+        string="Gender",
+    )
+    age_group = fields.Selection(
+        [
+            ("major", "Major"),
+            ("minor", "Minor"),
+        ],
+        string="Age Group",
+        compute="set_age_group",
+        store=True,
+    )
+    new_name = fields.Many2one(comodel_name="res.partner", string="New Name")
+    sale_order_count = fields.Integer(
+        string="No. of Sale Orders", compute="_onchange_new_name", store=True
+    )
