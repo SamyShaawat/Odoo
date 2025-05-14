48b228aac78a23c559f8b2e549f38e9eb741def6
SamyShaawat <samy.shaawat@gmail.com>
2025-05-14 13:02:08 +0300

    meaningful commit message

---
+++ odoo-17.0/account_sale_access_rights/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "account_sale_access_rights",
+    'summary': """
+    """,
+    'description': """
+    """,
+    'category': 'Sale',
+    'version': '15.0',
+    'depends': ['sale','account'],
+    # always loaded
+    'data': [
+        'security/security.xml'
+    ],
+}
