a7a8a095f575fcfede301c0253af0dee488f84b8
SamyShaawat <samy.shaawat@gmail.com>
2025-05-29 16:05:12 +0300

    update module name and add new icon

---
+++ odoo-17.0/real_estate/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    "name": "Real Estate",
+    "version": "17.0.1.0.0",
+    "category": "Extra Tools",
+    "author": "Samy Mostafa Shaawat",
+    "depends": [
+        "base",
+        "website",
+        "mail",
+        "sale_management",
+        "stock",
+        "contacts",
+        "web_enterprise",
+        "account",
+        "account_accountant",
+        "crm",
+        "purchase"
+    ],
+    "data": [
+        "views/base_menu.xml",
+        "views/property_view.xml",
+        "security/ir.model.access.csv"
+    ],
+    "installable": True,
+    "application": True,
+}
--- odoo-17.0/real_estate/models/__init__.py
+++ odoo-17.0/real_estate/models/__init__.py
@@ -1,5 +1,5 @@
+from . import property
 from . import model_one
 from . import model_two
 from . import model_three
 from . import model_four
-from . import property
